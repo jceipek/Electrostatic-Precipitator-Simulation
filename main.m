91b8a6e80099cfa9477cc869ce58d8864af3373e
Julian Ceipek <julian.ceipek@gmail.com>
2011-03-09 12:55:01 -0500

    Added analysis results.

--- main.m
+++ main.m
@@ -19,7 +19,12 @@ wireConfig = WireConfiguration(wire1,wire2);
 % wireConfig = WireConfiguration();
 
 %Particles
-particleCount = 20;
+particleCount = 50;
+collected = 0;
+undecided = 0;
+notCollected = 0;
+totalTime = 0;
+timeElapsed = 0;
 particles = generateParticlesForBombard(plateConfig,particleCount);
 
 %position = [0.1,-plateWidth/2,0];
@@ -30,22 +35,42 @@ plateConfig.plotPlates();
 hold on;
 wireConfig.plotWires();
 
-
-%Simulate and time a single particle
-
+%Simulate all of the particles
 for particlei = 1:particleCount
-
+    %Simulate and time a single particle
     particles{particlei}.plotParticleState();
     hold on;
     tic
     [T,W,particles{particlei}] = ndParticleSim(particles{particlei}...
                                 ,plateConfig,wireConfig,duration,...
                                 tolerance,1);
-    toc
+    timeElapsed = toc;
+    totalTime = totalTime + timeElapsed;
+    
     particles{particlei}.plotParticleState();
 
     plot3(W(:,1),W(:,2),W(:,3));
 
+    %Count the particles that were collected and survived
+    switch particles{particlei}.wasCollected 
+        case -1
+            undecided = undecided + 1;
+        case 0
+            notCollected = notCollected + 1;
+        case 1
+            collected = collected + 1;
+        otherwise
+            error('This should be impossible!');
+    end
+
+    clc
+    disp('#############RESULTS:#############')
+    fprintf('Undecided (this is bad): %i\n',undecided);
+    fprintf('Not Collected: %i\n',notCollected);
+    fprintf('Collected: %i\n',collected);
+    fprintf('Total vs Initial Count: %i/%i\n',notCollected+collected,particleCount);
+    fprintf('Time for Sim: %fs\n',timeElapsed);
+    fprintf('Total Time: %fs\n',totalTime);
 end
 
 %hold on;
